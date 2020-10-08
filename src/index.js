fc323b7042f4143486f1ae143185168685ea6895
angoulema <eugenia.labokha@yandex.by>
2020-10-08 16:06:13 +0300

    feat: add solution

--- src/index.js
+++ src/index.js
@@ -37,10 +37,34 @@ const MORSE_TABLE = {
     '-----':  '0',
 };
 
+
 function decode(expr) {
     // write your solution here
+
+    let arr = [];
+    let arrFound = [];
+    let n = expr.length / 10; //длина строки в декодированных символах
+    for (let i = 0; i < n; i++) {
+        arr[i] = expr.substring(0, 10); // массив из символов в двоичных числах
+        expr = expr.slice(10);
+    };
+    
+    for (let i = 0; i < n; i++) {
+        if (arr[i] === "**********") {
+          arrFound[i] = " ";
+        } else {
+          arr[i] = arr[i].replace(/10/gi, ".").replace(/11/gi, "-").replace(/00/gi, ''); //замена строки на .-
+          let k = arr[i];
+          arrFound[i] = MORSE_TABLE[k];
+        };
+      };
+
+
+    return arrFound.join("");
 }
 
 module.exports = {
     decode
 }
+
+
